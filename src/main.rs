93870a685ded01175d6ac9aa96637fef3cfeff5c
iambasanta <sangpangbasanta@gmail.com>
2023-06-19 20:05:00 +0545

    Refactor the configuration

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,39 @@
 use std::env;
 use std::fs;
+use std::process;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let query = &args[1];
-    let file_path = &args[2];
 
-    println!("Search for {}", query);
-    println!("In file {}", file_path);
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {err}");
+        process::exit(1);
+    });
 
-    let conents = fs::read_to_string(file_path)
+    println!("Search for {}", config.query);
+    println!("In file {}", config.file_path);
+
+    let conents = fs::read_to_string(config.file_path)
         .expect("Should have been abele to read the file");
 
     println!("With text:\n{conents}");
+}
+
+struct Config {
+    query: String,
+    file_path: String
+}
+
+impl Config {
+
+    fn build(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("not enough arguments")
+        }
+
+        let query = args[1].clone();
+        let file_path = args[2].clone();
+
+        Ok(Config{ query, file_path })
+    }
 }
